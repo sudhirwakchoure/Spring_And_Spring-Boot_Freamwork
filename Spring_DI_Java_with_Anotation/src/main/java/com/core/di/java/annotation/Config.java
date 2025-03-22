654c47ed9ada060d63fbd55c39e734b232974ba5
sudhir dattatray wakchoure <103427527+sudhirwakchoure@users.noreply.github.com>
2025-03-22 20:25:11 +0530

    Update Config.java

--- Spring_DI_Java_with_Anotation/src/main/java/com/core/di/java/annotation/Config.java
+++ Spring_DI_Java_with_Anotation/src/main/java/com/core/di/java/annotation/Config.java
@@ -3,6 +3,7 @@ package com.core.di.java.annotation;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+// @Configuration it say the this is confugration class
 @Configuration
 @ComponentScan(basePackages = "com.core.di.java.annotation")
 public class Config {
