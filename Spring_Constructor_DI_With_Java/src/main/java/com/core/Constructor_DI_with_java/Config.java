c5095cd460643ad05ccf4676692a18c9c58b4923
sudhir dattatray wakchoure <103427527+sudhirwakchoure@users.noreply.github.com>
2025-03-22 21:04:18 +0530

    Update Config.java

--- Spring_Constructor_DI_With_Java/src/main/java/com/core/Constructor_DI_with_java/Config.java
+++ Spring_Constructor_DI_With_Java/src/main/java/com/core/Constructor_DI_with_java/Config.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
  */
 
 /* suppose you have  multiple packages in you  class then you just give the base package like 
-   here base package is    "com.core"  insted of wrting this 
+   here base package is    "com.core"   instead of writing this
    @ComponentScan(basePackages = "com.core.Constructor_DI_with_java")
     you can write like 
     @ComponentScan(basePackages = "com.core")
